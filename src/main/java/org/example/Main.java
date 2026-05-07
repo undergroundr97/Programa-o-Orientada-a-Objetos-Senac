ea3ecfff872be35813e960579a7e106a5d57f4e6
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2026-05-07 19:34:03 -0300

    feat: Gerar numeros aleatorios para o CVV e o Numero do cartao

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,27 @@ package org.example;
 
 public class Main {
     static void main() {
-
+            String digitosCartao = "";
+        for (int i = 0; i < 4 ; i++) {
+            int quatroDigitosAleatorios = (int) (Math.random() * 1000);
+            String digitos = String.valueOf(quatroDigitosAleatorios);
+            if(digitos.length() <= 3){
+               digitos = String.format("%4s", digitos).replace(' ', '0');
+            }
+            digitosCartao += digitos + " ";
+        }
+        String cvv = "";
+        for (int i = 0; i < 1 ; i++) {
+            int tresDigitosAleatorios = (int) (Math.random() * 1000);
+            String digitos = String.valueOf(tresDigitosAleatorios);
+            if(digitos.length() < 3){
+                digitos = String.format("%3s", digitos).replace(' ', '0');
+            }
+            cvv = digitos;
+        }
+        System.out.println("CVV: " + cvv);
+        System.out.println("NUMERO CARTAO " + digitosCartao);
+//        CartaoCredito cartaoCredito = new CartaoCredito(digitosCartao, "Vitor", 5000.0, "331" )
 
     }
 }
