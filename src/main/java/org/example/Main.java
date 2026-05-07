2ff3fa26e1e709ac12318ce40eac8571ef97b156
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2026-05-07 19:49:07 -0300

    fix: Aumentar visibilidade no console

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 
 public class Main {
     static void main() {
-            String digitosCartao = "";
+        String digitosCartao = "";
         for (int i = 0; i < 4 ; i++) {
             int quatroDigitosAleatorios = (int) (Math.random() * 1000);
             String digitos = String.valueOf(quatroDigitosAleatorios);
@@ -21,17 +21,21 @@ public class Main {
             }
             cvv = digitos;
         }
-        System.out.println("CVV: " + cvv);
-        System.out.println("NUMERO CARTAO " + digitosCartao);
         CartaoCredito cartaoCredito = new CartaoCredito(digitosCartao, "Vitor", 5000.0, cvv );
         ProcessadorPegamento processadorPegamento = new ProcessadorPegamento(cartaoCredito);
-        System.out.println("Realizar venda de 4000, saldo atual: " + cartaoCredito.getLimiteDisponivel());
+        System.out.println("-------------------------------");
+        System.out.println("Realizar venda de R$2000.0, saldo atual: R$" + cartaoCredito.getLimiteDisponivel());
         processadorPegamento.executarVenda(2000.0);
-        System.out.println("Saldo atual do cartão: " + cartaoCredito.getLimiteDisponivel());
-        System.out.println("Realizar a venda de 4000, saldo atual: " + cartaoCredito.getLimiteDisponivel());
+        System.out.println("Saldo atual do cartão: R$" + cartaoCredito.getLimiteDisponivel());
+        System.out.println("-------------------------------");
+        System.out.println("Realizar a venda de R$4000.0, saldo atual: R$" +cartaoCredito.getLimiteDisponivel());
         processadorPegamento.executarVenda(4000.0);
+        System.out.println("-------------------------------");
         cartaoCredito.dadosCartao();
-
+        System.out.println("-------------------------------");
+        System.out.println("Estornando R$5000.0");
+        processadorPegamento.estornarVenda(5000.0);
+        System.out.println("Novo limite: R$" + cartaoCredito.getLimiteDisponivel());
 
     }
 }
