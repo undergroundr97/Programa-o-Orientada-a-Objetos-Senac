c03b901a08bf6aea461fd5bf9894e970e11eb39a
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2026-05-19 08:39:29 -0300

    feat(inputvalidator): adiciona validacoes de input

---
+++ src/main/java/org/example/InputValidator/InputValidator.java
@@ -0,0 +1,48 @@
+package org.InputValidor;
+
+import java.util.Scanner;
+
+public class InputValidator {
+    public static Scanner scanner = new Scanner(System.in);
+    public static boolean intValidator(Scanner scanner){
+        while(!scanner.hasNextInt()){
+            System.out.println("Input invalido");
+            scanner.next();
+        }
+        return true;
+    }
+
+    public static boolean doubleValidator(Scanner scanner){
+        while(!scanner.hasNextDouble()){
+            System.out.println("Input invalido");
+            scanner.next();
+        }
+        return true;
+    }
+
+    public static String yesNoValidator(String yesNo){
+        while(!yesNo.equalsIgnoreCase("s") && !yesNo.equalsIgnoreCase("n")){
+            System.out.println("Input invalido, digite S/N");
+            yesNo = scanner.next();
+        }
+        return yesNo;
+    }
+
+    public static Integer inputInRange0to4(Integer input){
+        int inp = input;
+        while(inp < 0 || inp > 4){
+            System.out.println("Input Invalido");
+            InputValidator.intValidator(scanner);
+            inp = scanner.nextInt();
+        }
+        return inp;
+    }
+
+
+    public static Integer getClienteInput(){
+        InputValidator.intValidator(scanner);
+        Integer input = scanner.nextInt();
+        scanner.nextLine();
+        return input;
+    }
+}
