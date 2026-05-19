5b8c4a568ee3c974296deec177451dc0f728303e
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2026-05-19 13:11:22 -0300

    fix(vitacaremenu): remove frase boasvindas, modifica opcao 2 para resumo

---
+++ src/main/java/org/example/Delay/DelayTimer.java
@@ -0,0 +1,4 @@
+package org.example.Delay;
+
+public class DelayTimer {
+}
--- src/main/java/org/example/VitaCare/VitaCareMenu.java
+++ src/main/java/org/example/VitaCare/VitaCareMenu.java
@@ -2,9 +2,8 @@ package org.example.VitaCare;
 
 public class VitaCareMenu {
     public static void exibirMenu(){
-        System.out.println("----------Bem Vindo ao VitaCare----------");
         System.out.println("1 - Cadastrar Novo Beneficiario");
-        System.out.println("2 - Calcular Mensalidade de um Beneficiario");
+        System.out.println("2 - Gerar Resumo para um Titular");
         System.out.println("3 - Verificar Coberturas de um Beneficiario");
         System.out.println("4 - Agendar Consulta para Beneficiario");
         System.out.println("5 - Verificar exames agendados no sistema");
