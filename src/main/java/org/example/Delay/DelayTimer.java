4c3f0d204a94cede27d66866dcf7bfc9a80f912c
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2026-05-19 13:12:01 -0300

    feat(delaytimer): adiciona classe delay para reutilizacao de bloco trycatch

--- src/main/java/org/example/Delay/DelayTimer.java
+++ src/main/java/org/example/Delay/DelayTimer.java
@@ -1,4 +1,11 @@
 package org.example.Delay;
 
 public class DelayTimer {
+    public static void delay(Integer mili){
+        try {
+            Thread.sleep(mili);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
